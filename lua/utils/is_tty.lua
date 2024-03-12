7b176ab4c1be27d03f6b3b5882502b103d948a75
aarondill <aaronsacks2006@gmail.com>
2024-03-12 05:12:21 -0500

    fix: remove sum unused utils
    These may be added back as I need them

--- lua/utils/is_tty.lua
+++
@@ -1,8 +0,0 @@
----returns a boolean indicating whether the current nvim session is running in a tty
----@return boolean is_tty true if running in a tty, false otherwise
-return function()
-  -- false in a SSH connection (likely in a terminal emulator)
-  if vim.env.SSH_CLIENT or vim.env.SSH_TTY then return false end
-  -- false if display is defined, else true
-  return not vim.env.DISPLAY
-end
