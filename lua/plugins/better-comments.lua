c3690cdd6830e7c17f96f7378f82bd551b00d0b0
aarondill <aaronsacks2006@gmail.com>
2024-03-11 21:58:03 -0500

    feat: add better-comments.nvim

---
+++ lua/plugins/better-comments.lua
@@ -0,0 +1,9 @@
+require("utils.pr_merged").on_lazy("Djancyp/better-comments.nvim", { 19, 18, })
+return {
+  "aarondill/better-comments.nvim",
+  opts = {
+    tags = {
+      { match = "!", fg = "#f44747", bg = "", bold = true, virtual_text = "" },
+    },
+  },
+}
