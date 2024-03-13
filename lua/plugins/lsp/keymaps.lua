815f889b8fdfc722ba1bfe55e11b3a69ced4c451
aarondill <aaronsacks2006@gmail.com>
2024-03-13 04:38:25 -0500

    feat: lsp keymaps

--- lua/plugins/lsp/keymaps.lua
+++ lua/plugins/lsp/keymaps.lua
@@ -11,6 +11,7 @@ local function has(buffer, method)
   end
   return false
 end
+local function has_plugin(p) return require("lazy.core.config").spec.plugins[p] ~= nil end
 
 ---@param client integer|vim.lsp.Client
 ---@param buffer? integer
@@ -18,44 +19,54 @@ function M.apply(client, buffer)
   if type(client) == "number" then client = assert(vim.lsp.get_client_by_id(client)) end
   buffer = buffer or vim.api.nvim_get_current_buf()
   --- Apply universal lsp remaps here
+  local cap = client.server_capabilities or {}
+  local function telescope_builtin(key)
+    return function() return require("telescope.builtin")[key]({ reuse_win = true }) end
+  end
+  local source_action = function()
+    return vim.lsp.buf.code_action({ context = { only = { "source" }, diagnostics = {} } })
+  end
 
-  map("n", "<leader>cl", "<cmd>LspInfo<cr>", "Lsp Info", { buffer = buffer })
-  if has(buffer, "definition") then
-    map("n", "gd", function() --
-      return require("telescope.builtin").lsp_definitions({ reuse_win = true })
-    end, "Goto Definition", { buffer = buffer })
+  local rename_rhs, rename_is_expr = vim.lsp.buf.rename, false
+  if has_plugin("inc-rename.nvim") then -- we have inc-rename
+    rename_rhs = function() return (":%s %s"):format(require("inc_rename").config.cmd_name, vim.fn.expand("<cword>")) end
+    rename_is_expr = true
   end
-  map("n", "gr", "<cmd>Telescope lsp_references<cr>", "References")
-  map("n", "gD", vim.lsp.buf.declaration, "Goto Declaration")
-  map("n", "gI", function() --
-    return require("telescope.builtin").lsp_implementations({ reuse_win = true })
-  end, "Goto Implementation")
-  map("n", "gy", function() --
-    return require("telescope.builtin").lsp_type_definitions({ reuse_win = true })
-  end, "Goto T[y]pe Definition")
-  map("n", "K", vim.lsp.buf.hover, "Hover")
-  if has(buffer, "signatureHelp") then
-    map("n", "gK", vim.lsp.buf.signature_help, "Signature Help")
-    -- map("i", "<c-k>", vim.lsp.buf.signature_help, "Signature Help")
-  end
-  if has(buffer, "codeAction") then
-    map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code Action")
-    local source_action = function()
-      return vim.lsp.buf.code_action({ context = { only = { "source" }, diagnostics = {} } })
+
+  ---@type ({mode?:string[]|string,[1]: string|string[], [2]: string|fun():any?, desc:string, cond: any})[]
+  local keys = {
+    { "<leader>cl", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
+    {
+      "gd",
+      telescope_builtin("lsp_definitions"),
+      desc = "Goto Definition",
+      buffer = buffer,
+      cond = cap.definitionProvider,
+    },
+    { "gD", vim.lsp.buf.declaration, desc = "Goto declaration", cond = cap.declarationProvider },
+    { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References", cond = cap.referencesProvider },
+    { "gI", telescope_builtin("lsp_implementations"), desc = "Goto Implementation" },
+    { "gy", telescope_builtin("lsp_type_definitions"), desc = "Goto T[y]pe Definition" },
+    { "K", vim.lsp.buf.hover, desc = "Hover", cond = cap.hoverProvider },
+    { "gK", vim.lsp.buf.signature_help, desc = "Signature Help", cond = cap.signatureHelpProvider },
+    { "<leader>ca", vim.lsp.buf.code_action, "Code Action", mode = { "n", "v" }, cond = cap.codeActionProvider },
+    { "<leader>cA", source_action, "Source Action", cond = cap.codeActionProvider },
+    { "<leader>cc", vim.lsp.codelens.run, desc = "Run Codelens", mode = { "n", "v" }, cond = cap.codeLensProvider },
+    { "<leader>cC", vim.lsp.codelens.refresh, desc = "Refresh & Display Codelens", cond = cap.codeLensProvider },
+    { { "<leader>cr", "<f2>" }, rename_rhs, desc = "Rename", expr = rename_is_expr, cond = cap.renameProvider },
+  }
+
+  for _, key in ipairs(keys) do
+    local mode, lhs, rhs, cond = key.mode, key[1], key[2], key.cond
+    if cond then
+      local opts = {}
+      for k, v in pairs(key) do
+        opts[k] = v
+      end
+      opts.buffer = buffer
+      opts.mode, opts[1], opts[2], opts.cond = nil, nil, nil, nil
+      map(mode or "n", lhs, rhs, opts)
     end
-    map("n", "<leader>cA", source_action, "Source Action")
-  end
-  if has(buffer, "codeLens") then
-    map({ "n", "v" }, "<leader>cc", vim.lsp.codelens.run, "Run Codelens")
-    map("n", "<leader>cC", vim.lsp.codelens.refresh, "Refresh & Display Codelens")
-  end
-  if has(buffer, "rename") then
-    local f, expr = vim.lsp.buf.rename, false
-    if require("lazy.core.config").spec.plugins["inc-rename.nvim"] then -- we have inc-rename
-      f = function() return (":%s %s"):format(require("inc_rename").config.cmd_name, vim.fn.expand("<cword>")) end
-      expr = true
-    end
-    map("n", { "<leader>cr", "<f2>" }, f, "Rename", { expr = expr })
   end
 end
 
