a3e03d5f6718c8ab8922346f4636fe495f277007
aarondill <aaronsacks2006@gmail.com>
2024-03-14 00:34:19 -0500

    feat: add <leader>gf keymap to open lazygit with the file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -154,6 +154,12 @@ map("n", "<leader>uT", function()
 end, "Toggle Treesitter Highlight")
 local lg = function() return require("utils.terminal").open("lazygit", { esc_esc = false, ctrl_hjkl = false }) end
 map("n", "<leader>gg", lg, "Lazygit")
+map("n", "<leader>gf", function()
+  if not vim.fn.executable("git") then return notifications.warn("git is not installed!") end
+  local pres = vim.system({ "git", "ls-files", "--full-name", vim.api.nvim_buf_get_name(0) }):wait()
+  local git_path = assert(pres.stdout, "stdout is not present")
+  require("utils.terminal").open({ "lazygit", "-f", vim.trim(git_path) }, { esc_esc = false, ctrl_hjkl = false })
+end, { desc = "Lazygit current file history" })
 -- windows
 map("n", "<leader>ww", "<C-W>p", "Other window", { remap = true })
 map("n", "<leader>wd", "<C-W>c", "Delete window", { remap = true })
