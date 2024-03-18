96d2ce2fb24599dff79d28430b3cdafd54de3aa5
aarondill <aaronsacks2006@gmail.com>
2024-03-18 07:02:56 -0500

    fix: add neodev types to lua_ls on nvim >=0.10

--- lua/plugins/lsp/servers/lua_ls.lua
+++ lua/plugins/lsp/servers/lua_ls.lua
@@ -1,6 +1,21 @@
-local library = {}
 --- TODO: remove this when possible!
-if vim.fn.has("nvim-0.10") == 1 then library[#library + 1] = (require("neodev.config").types()) end
+vim.api.nvim_create_autocmd("LspAttach", {
+  once = true, -- we don't need to modify the table more than once
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client or client.name ~= "lua_ls" then return end
+    local has_neodev, neodev = pcall(require, "neodev.config")
+    if not has_neodev then return end
+    local neodev_types = assert(neodev.types()) ---@type string
+    ---@type lspconfig.options.lua_ls|vim.lsp.ClientConfig
+    client.config =
+      vim.tbl_deep_extend("keep", client.config, { settings = { Lua = { workspace = { library = {} } } } })
+    --- Already has it :)
+    if vim.tbl_contains(client.config.settings.Lua.workspace.library, neodev_types) then return end
+    table.insert(client.config.settings.Lua.workspace.library, neodev_types)
+    assert(client.notify("workspace/didChangeConfiguration"), "Lsp server has shutdown!")
+  end,
+})
 
 ---@type LazySpec
 return {
@@ -11,7 +26,7 @@ return {
       lua_ls = { ---@type lspconfig.options.lua_ls | {}
         settings = {
           Lua = { ---@type _.lspconfig.settings.lua_ls.Lua|{}
-            workspace = { checkThirdParty = false, library = library }, ---@type _.lspconfig.settings.lua_ls.Workspace | {}
+            workspace = { checkThirdParty = false }, ---@type _.lspconfig.settings.lua_ls.Workspace | {}
             codeLens = { enable = true },
             completion = { ---@type _.lspconfig.settings.lua_ls.Completion | {}
               postfix = "@", -- use @ to fix a mistake (default)
