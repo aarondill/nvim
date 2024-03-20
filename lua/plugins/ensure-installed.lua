fc96ab14110fdef6cce0f8d2cbd8b337b6022a73
aarondill <aaronsacks2006@gmail.com>
2024-03-20 02:36:35 -0500

    refactor: linting -> ensure-installed

---
+++ lua/plugins/ensure-installed.lua
@@ -0,0 +1,42 @@
+local mason_ensure_installed = vim.tbl_flatten({
+  { "lua-language-server", "bash-language-server", "cspell", "eslint-lsp", "eslint_d", "jdtls" },
+  { "json-lsp", "prettier", "shellcheck", "taplo", "typescript-language-server", "vim-language-server" },
+  { "vint", "alex", "gitlint" },
+})
+local treesitter_ensure_installed = { "dockerfile", "git_config", "jsdoc", "make", "toml", "vimdoc", "java" }
+
+---@type LazySpec
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      opts = vim.tbl_deep_extend("keep", opts or {}, { ensure_installed = {} })
+      vim.list_extend(opts.ensure_installed, treesitter_ensure_installed)
+      return opts
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts = vim.tbl_deep_extend("keep", opts or {}, { ensure_installed = {} })
+      vim.list_extend(opts.ensure_installed, mason_ensure_installed)
+      return opts
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = function(_, opts)
+      opts = vim.tbl_deep_extend("keep", opts or {}, {
+        linters_by_ft = { markdown = {}, text = {}, gitcommit = {} },
+        linters = {},
+      })
+      table.insert(opts.linters_by_ft.markdown, "alex")
+      table.insert(opts.linters_by_ft.text, "alex")
+      table.insert(opts.linters_by_ft.gitcommit, "gitlint")
+      return opts
+    end,
+  },
+}
