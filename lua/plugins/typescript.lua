bbc74fbd880d30eece59254c3d0da7ddd2c4cbd1
aarondill <aaronsacks2006@gmail.com>
2024-03-20 03:48:50 -0500

    feat: typescript setup

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -35,4 +35,10 @@ return {
       end)
     end,
   },
+  { -- add typescript to treesitter
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then vim.list_extend(opts.ensure_installed, { "typescript", "tsx" }) end
+    end,
+  },
 }
