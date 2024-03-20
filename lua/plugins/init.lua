8f63c61dcb46cea02d0e4f3e4507aa51c4e5ff44
aarondill <aaronsacks2006@gmail.com>
2024-03-20 03:17:14 -0500

    feat: misc plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,4 +5,21 @@ return {
   { "NMAC427/guess-indent.nvim", event = { "BufReadPost", "BufNewFile", "BufWritePre" }, opts = {} }, -- Guess the current file indention type
   { "wsdjeg/vim-fetch", event = { "BufNewFile", "VimEnter" } }, -- Allow line numbers in file names
   { "micarmst/vim-spellsync", event = { "BufReadPost", "BufNewFile", "BufWritePre" } }, -- Rebuild spell files on file open
+  { "nvim-tree/nvim-web-devicons", lazy = true }, -- icons
+  { "dstein64/vim-startuptime", cmd = "StartupTime", init = function() vim.g.startuptime_tries = 10 end }, -- measure startuptime
+  { "nvim-lua/plenary.nvim", lazy = true }, -- library used by other plugins
+
+  -- Session management. This saves your session in the background,
+  -- keeping track of open buffers, window arrangement, and more.
+  -- You can restore sessions when returning through the dashboard.
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = vim.opt.sessionoptions:get() },
+    keys = {
+      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+    },
+  },
 }
