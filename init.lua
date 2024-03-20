af6603523b316a1f682e955e0e0f13ac38218128
aarondill <aaronsacks2006@gmail.com>
2024-03-20 02:32:30 -0500

    fix: force nvim >= 0.9

--- init.lua
+++ init.lua
@@ -8,6 +8,16 @@ vim.filetype.add({
   },
 })
 
+if vim.fn.has("nvim-0.9.0") == 0 then
+  vim.api.nvim_echo({
+    { "This configurations requires Neovim >= 0.9.0\n", "ErrorMsg" },
+    { "Aborting configuration. You will be left with Vanilla Neovim.\n", "ErrorMsg" },
+    { "Press any key to continue.", "MoreMsg" },
+  }, true, {})
+  vim.fn.getchar()
+  return
+end
+
 require("config.options") -- This needs to come first!
 require("config.lazy") -- bootstrap lazy.nvim and plugins
 
