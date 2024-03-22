24174fa1d7a49c9755ac1e47ee8e7a6b8c6f5512
aarondill <aaronsacks2006@gmail.com>
2024-03-22 09:19:38 -0500

    fix: Remove LspAttach hack to all neodev types
    This is already fixed by https://github.com/folke/neodev.nvim/commit/f54ec26

--- lua/plugins/lsp/servers/lua_ls.lua
+++ lua/plugins/lsp/servers/lua_ls.lua
@@ -1,22 +1,3 @@
---- TODO: remove this when possible!
-vim.api.nvim_create_autocmd("LspAttach", {
-  once = true, -- we don't need to modify the table more than once
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if not client or client.name ~= "lua_ls" then return end
-    local has_neodev, neodev = pcall(require, "neodev.config")
-    if not has_neodev then return end
-    local neodev_types = assert(neodev.types()) ---@type string
-    ---@type lspconfig.options.lua_ls|vim.lsp.ClientConfig
-    client.config =
-      vim.tbl_deep_extend("keep", client.config, { settings = { Lua = { workspace = { library = {} } } } })
-    --- Already has it :)
-    if vim.tbl_contains(client.config.settings.Lua.workspace.library, neodev_types) then return end
-    table.insert(client.config.settings.Lua.workspace.library, neodev_types)
-    assert(client.notify("workspace/didChangeConfiguration"), "Lsp server has shutdown!")
-  end,
-})
-
 ---@type LazySpec
 return {
   "neovim/nvim-lspconfig",
