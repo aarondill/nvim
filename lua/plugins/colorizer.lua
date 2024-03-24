e7f953458ca3eb38c35c6e9340db2c9cf127c9cd
aarondill <aaronsacks2006@gmail.com>
2024-03-24 02:24:14 -0500

    fix(colorizer): don't include on ignored filetypes

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,8 +1,14 @@
+local consts = require("consts")
+local ignored = vim.tbl_map(function(ft) return "!" .. ft end, consts.ignored_filetypes)
 ---@type LazySpec
 return {
   "NvChad/nvim-colorizer.lua",
   cond = vim.o.termguicolors,
   opts = {
+    filetypes = {
+      "*",
+      unpack(ignored),
+    },
     user_default_options = {
       names = false,
       RRGGBBAA = true,
