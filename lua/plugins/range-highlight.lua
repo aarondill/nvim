7e585623cc2f5b480fa22b70d9ff3198f79cf416
aarondill <aaronsacks2006@gmail.com>
2024-03-25 15:36:59 -0500

    fix: remove range-highlight
    it causes mappings to lag

--- lua/plugins/range-highlight.lua
+++
@@ -1,7 +0,0 @@
----@type LazySpec
-return {
-  "winston0410/range-highlight.nvim",
-  dependencies = { "winston0410/cmd-parser.nvim" },
-  event = { "CmdlineChanged" },
-  opts = { highlight = "CurSearch" },
-}
