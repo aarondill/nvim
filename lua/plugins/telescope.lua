a2382dd0ea0c340445cc365d833f534ec142327a
aarondill <aaronsacks2006@gmail.com>
2024-03-25 03:04:38 -0500

    feat: smart open
    for now? we'll see how i like it

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -37,7 +37,7 @@ return {
   keys = {
     { "<leader>/", t.telescope("live_grep"), desc = "Grep (root dir)" },
     { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-    { "<leader><space>", t.telescope("files"), desc = "Find Files (root dir)" },
+    -- { "<leader><space>", t.telescope("files"), desc = "Find Files (root dir)" },
     -- find
     { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
     { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
