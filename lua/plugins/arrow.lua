26b83b7cc22d125200d4d19cf242851b514f816a
aarondill <aaronsacks2006@gmail.com>
2024-03-25 02:51:13 -0500

    map next/prev arrow

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -6,6 +6,10 @@ return {
     show_icons = not is_tty(),
     leader_key = ";", -- Recommended to be a single key
     save_key = "git_root",
+    mappings = {
+      next_item = "'",
+      prev_item = "l",
+    },
   },
   keys = function(self) return { self.opts and self.opts.leader_key or ";", mode = "n" } end,
 }
