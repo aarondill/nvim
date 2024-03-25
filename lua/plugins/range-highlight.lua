dd4ce5489c95ebf94607405c4348f74bd4f05971
aarondill <aaronsacks2006@gmail.com>
2024-03-25 11:44:23 -0500

    fix: change range-highlight to CurSearch to be more recognizable

--- lua/plugins/range-highlight.lua
+++ lua/plugins/range-highlight.lua
@@ -3,5 +3,5 @@ return {
   "winston0410/range-highlight.nvim",
   dependencies = { "winston0410/cmd-parser.nvim" },
   event = { "CmdlineChanged" },
-  opts = {},
+  opts = { highlight = "CurSearch" },
 }
