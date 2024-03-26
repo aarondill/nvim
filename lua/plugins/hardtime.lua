03cb07be97b01a52e6b69a27f58b44df222947f5
aarondill <aaronsacks2006@gmail.com>
2024-03-25 20:15:37 -0500

    fix: configure hardtime
    maybe now it'll be usable

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,7 +2,13 @@
 return {
    "m4xshen/hardtime.nvim",
    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-   opts = { disabled_filetypes = require("consts").ignored_filetypes },
+   opts = {
+      disabled_filetypes = require("consts").ignored_filetypes,
+      disable_mouse = false,
+      restriction_mode = "hint",
+      max_time = 100,
+      disabled_keys = { ["<Up>"] = {}, ["<Down>"] = {}, ["<Left>"] = {}, ["<Right>"] = {} },
+   },
    event = { "BufReadPre", "BufNewFile", "BufEnter" },
    cmd = "Hardtime",
 }
