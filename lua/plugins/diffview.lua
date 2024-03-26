cac0790a1be22cd89fbb765c762b1802a652f8fb
aarondill <aaronsacks2006@gmail.com>
2024-03-25 19:02:12 -0500

    fix: close on q

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,10 +1,12 @@
 local is_tty = require("utils.is_tty")
+local close = { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close the diffview" } }
 ---@type LazySpec
 return {
   "sindrets/diffview.nvim",
+  -- hello
   keys = {
     { "<leader>fh", "<cmd>DiffviewFileHistory %<cr>", desc = "[F]ile [H]istory" },
-    { "<leader>sd", "<cmd>DiffviewOpen<cr>", desc = "[S]how [D]iff" },
+    { "<leader>ss", "<cmd>DiffviewOpen<cr>", desc = "[S]how [S]tatus" },
   },
   cmd = {
     "DiffviewFileHistory",
@@ -16,5 +18,10 @@ return {
   },
   opts = {
     use_icons = not is_tty(),
+    keymaps = {
+      file_panel = { close },
+      view = { close },
+      file_history_panel = { close },
+    },
   },
 }
