7d6e3e107503d6f0660f1eb1f8461eed9299b21f
aarondill <aaronsacks2006@gmail.com>
2024-04-09 00:30:28 -0500

    feat: add autolist

---
+++ lua/plugins/autolist.lua
@@ -0,0 +1,20 @@
+local map = require("utils.map")
+return {
+  "gaoDean/autolist.nvim",
+  ft = { "markdown", "text", "tex", "plaintex", "norg" },
+  config = function()
+    require("autolist").setup()
+
+    map("i", "<tab>", "<cmd>AutolistTab<cr>")
+    map("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
+    map("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+    map("n", "o", "o<cmd>AutolistNewBullet<cr>")
+    map("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+    map("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+    -- functions to recalculate list on edit
+    for _, k in ipairs({ ">>", "<<", "dd" }) do
+      map("n", k, k .. "<cmd>AutolistRecalculate<cr>")
+    end
+    map("v", "d", "d<cmd>AutolistRecalculate<cr>")
+  end,
+}
