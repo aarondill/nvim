d97d23d0317354b859b090cdb2c00652cce02f88
aarondill <aaronsacks2006@gmail.com>
2024-05-22 01:23:18 -0500

    fix: temporarily change nvim-lightbulb to fix nvim 0.10

--- lua/plugins/nvim-lightbulb.lua
+++ lua/plugins/nvim-lightbulb.lua
@@ -1,7 +1,8 @@
 -- Show a lightbulb in the gutter when a code action is available
 ---@type LazySpec
 return {
-  "kosayoda/nvim-lightbulb",
+  "gh-liu/nvim-lightbulb", -- A fork to use until kosayoda/nvim-lightbulb#62 is fixed
+  -- "kosayoda/nvim-lightbulb",
   event = { "CursorHold", "CursorHoldI" },
   opts = {
     autocmd = { enabled = true },
