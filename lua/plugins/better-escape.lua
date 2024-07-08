c7c4628f2e43bb8606d880892b4bfc3f91889b7d
aarondill <aaronsacks2006@gmail.com>
2024-07-07 19:28:42 -0500

    fix: double update better-escape

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,15 +1,15 @@
+--- Ensure the cursor is where i expect it to be after escaping
+local function esc() return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<esc>l" or "<esc>" end
 ---@type LazySpec
 return {
   "max397574/better-escape.nvim",
   opts = {
     mappings = {
       i = {
-        j = { k = "<Esc>" },
-        k = { j = "<Esc>" },
+        j = { k = esc },
+        k = { j = esc },
       },
     },
-    --- Ensure the cursor is where i expect it to be after escaping
-    keys = function() return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<esc>l" or "<esc>" end,
   },
   event = { "InsertEnter" },
 }
