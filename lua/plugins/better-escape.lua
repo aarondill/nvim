344bff505f76d003e7f3b317eaf413649fcc51de
aarondill <aaronsacks2006@gmail.com>
2024-07-07 19:26:06 -0500

    fix: update better-escape

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -2,7 +2,12 @@
 return {
   "max397574/better-escape.nvim",
   opts = {
-    mapping = { "jk", "kj" },
+    mappings = {
+      i = {
+        j = { k = "<Esc>" },
+        k = { j = "<Esc>" },
+      },
+    },
     --- Ensure the cursor is where i expect it to be after escaping
     keys = function() return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<esc>l" or "<esc>" end,
   },
