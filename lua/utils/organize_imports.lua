16e7efcf6ac2563b9578134c4bc123e0f5b63dce
Aaron Dill <aaronsacks2006@gmail.com>
2024-07-20 02:15:13 -0500

    fix: organize imports sync

--- lua/utils/organize_imports.lua
+++ lua/utils/organize_imports.lua
@@ -1,6 +1,6 @@
 return function()
   local tsserver_is_attached = next(vim.lsp.get_clients({ bufnr = 0, name = "tsserver" })) ~= nil
-  return vim.lsp.buf.execute_command({
+  return vim.lsp.buf_request_sync(0, "workspace/executeCommand", {
     command = tsserver_is_attached and "_typescript.organizeImports" or "source.organizeImports",
     arguments = { vim.api.nvim_buf_get_name(0) },
   })
