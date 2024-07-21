e3d7e619ef07d41ffa0e2e48ad8ed6330781e4ed
Aaron Dill <aaronsacks2006@gmail.com>
2024-07-21 17:48:43 -0500

    fix: detect support for executeCommand

--- lua/utils/organize_imports.lua
+++ lua/utils/organize_imports.lua
@@ -1,7 +1,20 @@
 return function()
-  local tsserver_is_attached = next(vim.lsp.get_clients({ bufnr = 0, name = "tsserver" })) ~= nil
-  vim.lsp.buf_request_sync(0, "workspace/executeCommand", {
-    command = tsserver_is_attached and "_typescript.organizeImports" or "source.organizeImports",
-    arguments = { vim.api.nvim_buf_get_name(0) },
-  })
+  local bufnr = vim.api.nvim_get_current_buf()
+  local method = "workspace/executeCommand"
+
+  local tsserver_is_attached = next(vim.lsp.get_clients({ bufnr = bufnr, name = "tsserver" })) ~= nil
+  local command = tsserver_is_attached and "_typescript.organizeImports" or "source.organizeImports"
+
+  if not tsserver_is_attached then
+    local supports = false
+    for _, client in ipairs(vim.lsp.get_clients({ bufnr = bufnr })) do
+      if client.supports_method(method, { bufnr = bufnr }) then
+        supports = true
+        break
+      end
+    end
+    if not supports then return end -- This client does not support the workspace/executeCommand method
+  end
+
+  vim.lsp.buf_request_sync(bufnr, method, { command = command, arguments = { vim.api.nvim_buf_get_name(bufnr) } })
 end
