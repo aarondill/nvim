d9dc62f7e89115dd6cf325b7e91f5819c4943cab
Aaron Dill <aaronsacks2006@gmail.com>
2024-07-21 20:43:53 -0500

    refactor: organize_imports

--- lua/utils/organize_imports.lua
+++ lua/utils/organize_imports.lua
@@ -5,16 +5,9 @@ return function()
   local tsserver_is_attached = next(vim.lsp.get_clients({ bufnr = bufnr, name = "tsserver" })) ~= nil
   local command = tsserver_is_attached and "_typescript.organizeImports" or "source.organizeImports"
 
-  if not tsserver_is_attached then
-    local supports = false
-    for _, client in ipairs(vim.lsp.get_clients({ bufnr = bufnr })) do
-      if client.supports_method(method, { bufnr = bufnr }) then
-        supports = true
-        break
-      end
-    end
-    if not supports then return end -- This client does not support the workspace/executeCommand method
-  end
+  local supports = tsserver_is_attached
+    or next(vim.lsp.get_clients({ bufnr = bufnr, method = "workspace/executeCommand" })) ~= nil
+  if not supports then return end -- This client does not support the workspace/executeCommand method
 
   vim.lsp.buf_request_sync(bufnr, method, { command = command, arguments = { vim.api.nvim_buf_get_name(bufnr) } })
 end
