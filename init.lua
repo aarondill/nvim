2ebe5becb6c8815296884c6ddc307aa63016d8d9
Aaron Dill <aaronsacks2006@gmail.com>
2024-07-30 21:29:38 -0500

    fix: /etc/default/grub* should be shell scripts

--- init.lua
+++ init.lua
@@ -2,6 +2,12 @@ require("future") -- Fowards compatability
 
 -- Override .pluto and .tmpl extensions
 vim.filetype.add({
+  pattern = {
+    [".*/etc/default/grub.d/.*%.cfg"] = "sh",
+  },
+  filename = {
+    ["/etc/default/grub"] = "sh",
+  },
   extension = {
     [".pluto"] = "lua",
     [".tmpl"] = function(path) return vim.fs.basename(path):match(".+%.(.+).tmpl$") end,
