0c35fa995d6c133ae2e81c977579a12a6a7735c0
Aaron Dill <aaronsacks2006@gmail.com>
2024-08-02 16:07:07 -0500

    fix: function completion is TS

--- lua/plugins/lsp/servers/tsserver.lua
+++ lua/plugins/lsp/servers/tsserver.lua
@@ -1,14 +1,10 @@
 ---@type LazySpec
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      tsserver = {
-        settings = { ---@type lspconfig.settings.tsserver|{}
-          completions = {
-            completeFunctionCalls = true,
-          },
-        },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tsserver = {},
       },
     },
   },
