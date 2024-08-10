6da32e6db9dd809318c901412e67833a17ffce68
Aaron Dill <aaronsacks2006@gmail.com>
2024-08-09 23:51:57 -0500

    fix: add .nvimrc.lua to config-local

--- lua/plugins/config-local.lua
+++ lua/plugins/config-local.lua
@@ -4,7 +4,7 @@ return {
   main = "config-local",
   lazy = false,
   opts = {
-    config_files = { ".nvim.lua", ".nvimrc", ".exrc", ".nvim/init.lua" }, -- Config file patterns to load (lua supported)
+    config_files = { ".nvim.lua", ".nvimrc", ".nvimrc.lua", ".exrc", ".nvim/init.lua" }, -- Config file patterns to load (lua supported)
     lookup_parents = true, -- Lookup config files in parent directories
     -- silent = false, -- Disable plugin messages (Config loaded/ignored)
   },
