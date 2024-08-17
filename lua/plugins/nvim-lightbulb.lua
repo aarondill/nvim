8bb182fef333af8e867f397dbbffbe63436fbb19
Aaron Dill <aaronsacks2006@gmail.com>
2024-08-17 06:13:16 -0500

    fix: revert to upstream nvim-lightbulb

--- lua/plugins/nvim-lightbulb.lua
+++ lua/plugins/nvim-lightbulb.lua
@@ -1,8 +1,7 @@
 -- Show a lightbulb in the gutter when a code action is available
 ---@type LazySpec
 return {
-  "gh-liu/nvim-lightbulb", -- A fork to use until kosayoda/nvim-lightbulb#62 is fixed
-  -- "kosayoda/nvim-lightbulb",
+  "kosayoda/nvim-lightbulb",
   event = { "CursorHold", "CursorHoldI" },
   opts = {
     autocmd = { enabled = true },
