70ce6fbae53b3ff11c942bff26ff5db270a95828
Aaron Dill <aaronsacks2006@gmail.com>
2024-09-13 03:43:32 -0500

    fix: switch to nvim-lightbulb fork
    use fork until kosayoda/nvim-lightbulb#67 gets merged

--- lua/plugins/nvim-lightbulb.lua
+++ lua/plugins/nvim-lightbulb.lua
@@ -1,7 +1,8 @@
 -- Show a lightbulb in the gutter when a code action is available
 ---@type LazySpec
 return {
-  "kosayoda/nvim-lightbulb",
+  -- "kosayoda/nvim-lightbulb",
+  "Traap/nvim-lightbulb", -- a fork until PR #67 is merged
   event = { "CursorHold", "CursorHoldI" },
   opts = {
     autocmd = { enabled = true },
