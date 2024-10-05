b9152030f96e3b0ff6a7a2818d59b4809b6a9016
Aaron Dill <aaronsacks2006@gmail.com>
2024-10-04 21:38:17 -0500

    refactor: make organize_imports more extensible

--- lua/utils/organize_imports.lua
+++ lua/utils/organize_imports.lua
@@ -1,13 +1,25 @@
+---@alias organize_imports_command string|fun(self: vim.lsp.Client, bufnr: integer):string?
+
+---Mapping of lsp names (vim.lsp.get_clients) to commands. Special name
+---`default` is used if no key is found. If function, it is called and may
+---optionally return a string. If a string, passed as an argument to
+---`vim.lsp.buf_request_sync` with method `workspace/executeCommand`
+---@type table<string, organize_imports_command>
+local commands = {
+  default = "source.organizeImports",
+  ["ts_ls"] = "_typescript.organizeImports",
+  ["jdtls"] = function() return require("jdtls").organize_imports() end,
+}
+
 return function()
-  local bufnr = vim.api.nvim_get_current_buf()
   local method = "workspace/executeCommand"
+  local bufnr = vim.api.nvim_get_current_buf()
+  local client = vim.lsp.get_clients({ bufnr = bufnr })[1]
+  ---@type organize_imports_command?
+  local command = commands[client.name] or commands.default
+  if type(command) == "function" then command = command(client, bufnr) end
+  if not command then return end -- No command found / function returned nil
 
-  local ts_ls_is_attached = next(vim.lsp.get_clients({ bufnr = bufnr, name = "ts_ls" })) ~= nil
-  local command = ts_ls_is_attached and "_typescript.organizeImports" or "source.organizeImports"
-
-  local supports = ts_ls_is_attached
-    or next(vim.lsp.get_clients({ bufnr = bufnr, method = "workspace/executeCommand" })) ~= nil
-  if not supports then return end -- This client does not support the workspace/executeCommand method
-
-  vim.lsp.buf_request_sync(bufnr, method, { command = command, arguments = { vim.api.nvim_buf_get_name(bufnr) } })
+  if not client.supports_method(method, { bufnr = bufnr }) then return end -- This client does not support the workspace/executeCommand method
+  client.request_sync(method, { command = command, arguments = { vim.api.nvim_buf_get_name(bufnr) } }, nil, bufnr)
 end
