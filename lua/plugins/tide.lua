a8107a2d9af11621ed1239b9960609a8b3ba8707
Aaron Dill <aaronsacks2006@gmail.com>
2024-11-10 03:10:06 -0600

    feat!: replace harpoon with jackMort/tide.nvim

---
+++ lua/plugins/tide.lua
@@ -0,0 +1,17 @@
+---@type LazySpec
+return {
+  "jackMort/tide.nvim",
+  opts = {
+    keys = {
+      leader = ";",
+    },
+    animation_duration = 100, -- Animation duration in milliseconds
+  },
+  keys = function(self) return { (self.opts and self.opts.keys and self.opts.keys.leader) or ";" } end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    -- "nvim-tree/nvim-web-devicons",
+    --Webdevicons is replaced by mini.icons
+    "echasnovski/mini.icons",
+  },
+}
