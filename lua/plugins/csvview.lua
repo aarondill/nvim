670c4fba3a3c751efee68bca65f7d5613c0a2740
Aaron Dill <aaronsacks2006@gmail.com>
2025-02-19 23:15:54 -0600

    feat: decisive -> csvview. It's better for editing

---
+++ lua/plugins/csvview.lua
@@ -0,0 +1,29 @@
+local create_autocmd = require("utils.create_autocmd")
+---@type LazySpec
+return {
+  "hat0uma/csvview.nvim",
+  ---@module "csvview"
+  ---@type CsvView.Options
+  opts = {
+    parser = { comments = { "#", "//" } },
+    keymaps = {
+      -- Text objects for selecting fields
+      textobject_field_inner = { "if", mode = { "o", "x" } },
+      textobject_field_outer = { "af", mode = { "o", "x" } },
+      -- Use <Tab> and <S-Tab> to move horizontally between fields.
+      -- Use <Enter> and <S-Enter> to move vertically between rows.
+      jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+      jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+      jump_next_row = { "<Enter>", mode = { "n", "v" } },
+      jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+    },
+  },
+  init = function()
+    create_autocmd("FileType", function(ev)
+      if not vim.api.nvim_buf_is_valid(ev.buf) then return end
+      return require("csvview").enable(ev.buf)
+    end, "Enable csvview", { pattern = { "csv", "tsv" } })
+  end,
+  ft = { "csv", "tsv" },
+  cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+}
