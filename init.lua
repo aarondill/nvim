9320e1460a2a1681b7006f4f2557109e223a2c2b
Aaron Dill <aaronsacks2006@gmail.com>
2025-02-20 08:16:20 -0600

    fix: notificatoin timer should last longer

--- init.lua
+++ init.lua
@@ -14,14 +14,19 @@ package.cpath = package.cpath:gsub("^%./%?%.so;", "")
 
 require("future") -- Fowards compatability
 
+--- Wait until after config is loaded before starting notification timer
+local _start_notifs_timer ---@type function?
 do -- delay notifications till vim.notify was replaced or after 500ms
   local notifs, orig = {}, vim.notify
   local function temp(...) table.insert(notifs, vim.F.pack_len(...)) end
   vim.notify = temp
 
-  local timer, check = vim.uv.new_timer(), assert(vim.uv.new_check())
+  local timer, check = vim.uv.new_timer(), vim.uv.new_check()
+  assert(timer)
+  assert(check)
   local function replay()
     _ = { timer:stop(), timer:close(), check:stop(), check:close() } -- clean up the timer and check
+    timer, check = nil, nil
     if vim.notify == temp then vim.notify = orig end -- put back the original notify if needed
     vim.schedule(function()
       for _, notif in ipairs(notifs) do
@@ -33,7 +38,11 @@ do -- delay notifications till vim.notify was replaced or after 500ms
   check:start(function()
     if vim.notify ~= temp then replay() end
   end) -- wait till vim.notify has been replaced
-  timer:start(500, 0, replay) -- or if it took more than 500ms, then something went wrong
+  _start_notifs_timer = function()
+    if not timer then return end -- If this is called after the check has fired, then we're good
+    timer:start(500, 0, replay) -- or if it took more than 500ms, then something went wrong
+    _start_notifs_timer = nil -- only run this once/gc
+  end
 end
 
 require("config.options") -- This needs to come first!
@@ -48,3 +57,5 @@ if rt and vim.loop.fs_access(rt, "W") then
   --- Regen the helptags
   vim.cmd.helptags(vim.fs.joinpath(rt, "doc"))
 end
+
+vim.schedule(_start_notifs_timer) -- start the timer after we're done with everything
