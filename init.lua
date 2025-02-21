84dfde0eeeac1b2c747eb57a809454bfa9921cf5
Aaron Dill <aaronsacks2006@gmail.com>
2025-02-21 07:16:07 -0600

    feat: keymap notify

--- init.lua
+++ init.lua
@@ -32,6 +32,7 @@ do -- delay notifications till vim.notify was replaced or after 500ms
       for _, notif in ipairs(notifs) do
         vim.notify(vim.F.unpack_len(notif))
       end
+      notifs = nil
     end)
   end
 
@@ -57,5 +58,46 @@ if rt and vim.loop.fs_access(rt, "W") then
   --- Regen the helptags
   vim.cmd.helptags(vim.fs.joinpath(rt, "doc"))
 end
+
+---@class nvim_buf_get_keymap.Keymap
+---@field abbr 0|1,
+---@field buffer 0|1,
+---@field desc? string,
+---@field expr 0|1,
+---@field lhs string,
+---@field lhsraw string,
+---@field lnum 0|1,
+---@field mode? string,
+---@field mode_bits integer,
+---@field noremap 0|1,
+---@field nowait 0|1,
+---@field rhs? string,
+---@field callback? function,
+---@field script 0|1,
+---@field scriptversion integer,
+---@field sid integer,
+---@field silent 0|1,
+
+vim.schedule(function()
+  ---@type nvim_buf_get_keymap.Keymap[]
+  local keys = vim
+    .iter({ vim.api.nvim_buf_get_keymap(0, ""), vim.api.nvim_get_keymap("") })
+    :flatten(1)
+    :filter(function(k) ---@param k nvim_buf_get_keymap.Keymap
+      return k.lhs ~= "" and not k.lhs:find("^<Plug>") -- exclude plug mappings
+    end)
+    :filter(function(k) ---@param k nvim_buf_get_keymap.Keymap
+      local has_rhs = k.rhs ~= nil and (k.rhs ~= "" and k.rhs:upper() ~= "<NOP>") -- exclude empty mappings
+      local has_desc = k.desc ~= nil -- exclude empty descriptions
+      local has_callback = k.callback ~= nil -- exclude empty callbacks
+      if not has_rhs and not has_callback then return false end -- if this mapping doesn't do anything, don't show it
+      return has_desc or has_rhs -- Only show it if it has a description or a rhs (callbacks aren't displayable)
+    end)
+    :totable()
+  local i = math.random(1, #keys)
+  local k = keys[i]
+  local msg = ('%smap "%s"'):format(k.mode or "", k.lhsraw) .. ": " .. (k.desc or k.rhs)
+  require("utils.notifications").info(msg, { title = "Keymap", timeout = 5 * 1000 })
+end)
 
 vim.schedule(_start_notifs_timer) -- start the timer after we're done with everything
