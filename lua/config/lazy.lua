9e8cca0d1ca5b7bb11dbf7d87548c92aef71389a
Aaron Dill <aaronsacks2006@gmail.com>
2025-02-24 11:51:53 -0600

    fix: disable change_detection

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -84,6 +84,7 @@ end, "Close lazy.nvim when done installing", { pattern = "LazyDone", once = true
 local icons = require("config.icons")
 require("lazy").setup({
   ui = { icons = icons.lazy_nvim.ui.icons },
+  change_detection = { enabled = false },
   ---@type table -- Force it. The type is wrong.
   dev = {
     -- directory where you store your local plugin projects
