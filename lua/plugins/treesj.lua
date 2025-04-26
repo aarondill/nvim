c26989e625a197efc4d2a7151452e5446348ee61
Aaron Dill <aaronsacks2006@gmail.com>
2025-04-25 19:09:30 -0500

    fix: desc treesj

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -2,9 +2,9 @@
 return {
   "Wansmer/treesj",
   keys = {
-    { "<leader>m", function() require("treesj").toggle() end },
-    { "<leader>jj", function() require("treesj").join() end },
-    { "<leader>js", function() require("treesj").split() end },
+    { "<leader>m", function() require("treesj").toggle() end, desc = "Toggle TreeSJ" },
+    { "<leader>jj", function() require("treesj").join() end, desc = "Join TreeSJ" },
+    { "<leader>js", function() require("treesj").split() end, desc = "Split TreeSJ" },
   },
   opts = { use_default_keymaps = false, max_join_length = 200 },
 }
