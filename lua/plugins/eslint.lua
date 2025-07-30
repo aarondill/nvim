5a0b096631747c3e0d02eb8bead35a901fdc52f9
Aaron Dill <aaronsacks2006@gmail.com>
2025-07-30 16:04:37 -0500

    fix: re-enable the eslint formatter

--- lua/plugins/eslint.lua
+++ lua/plugins/eslint.lua
@@ -1,42 +1,48 @@
-if true then return {} end
+local create_autocmd = require("utils.create_autocmd")
+-- if true then return {} end
+
+create_autocmd("LspAttach", function(ev)
+  do
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if not client or client.name ~= "eslint" then return end
+  end
+
+  local function get_client(buf) return vim.lsp.get_clients({ name = "eslint", bufnr = buf })[1] end
+
+  local formatter = require("plugins.lsp.formatter").formatter({
+    name = "Eslint: LSP",
+    primary = false,
+    priority = 200,
+    filter = "eslint",
+  })
+
+  -- Use EslintFixAll on Neovim < 0.10.0
+  if vim.fn.has("nvim-0.10.0") == 0 then
+    formatter.name = "eslint: EslintFixAll"
+    formatter.sources = function(buf) return get_client(buf) and { "eslint" } or {} end
+    formatter.format = function(buf)
+      local client = get_client(buf)
+      if not client then return end
+      local diag = vim.diagnostic.get(buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+      if #diag > 0 then vim.cmd("EslintFixAll") end
+    end
+  end
+
+  -- register the formatter
+  require("utils.format").register(formatter)
+end)
+
 ---@type LazySpec
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    servers = { ---@type lspconfig.options|{}
+    servers = {
       eslint = {
-        settings = { ---@type lspconfig.settings.eslint|{}
+        settings = {
           -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
           workingDirectories = { mode = "auto" },
         },
       },
-    },
-    setup = {
-      eslint = function()
-        local function get_client(buf) return vim.lsp.get_clients({ name = "eslint", bufnr = buf })[1] end
-
-        local formatter = require("plugins.lsp.formatter").formatter({
-          name = "eslint: lsp",
-          primary = false,
-          priority = 200,
-          filter = "eslint",
-        })
-
-        -- Use EslintFixAll on Neovim < 0.10.0
-        if not pcall(require, "vim.lsp._dynamic") then
-          formatter.name = "eslint: EslintFixAll"
-          formatter.sources = function(buf) return get_client(buf) and { "eslint" } or {} end
-          formatter.format = function(buf)
-            local client = get_client(buf)
-            if not client then return end
-            local diag = vim.diagnostic.get(buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
-            if #diag > 0 then vim.cmd("EslintFixAll") end
-          end
-        end
-
-        -- register the formatter
-        require("util.format").register(formatter)
-      end,
     },
   },
 }
