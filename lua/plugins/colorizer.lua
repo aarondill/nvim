3191751ea072ff2846a8e3926e583607f62f83cd
Aaron Dill <aaronsacks2006@gmail.com>
2026-02-28 12:27:08 -0600

    fix: update colorizer config

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,20 +1,32 @@
 local consts = require("consts")
+if false then require("colorizer") end -- import for types
+
 local ignored = vim.tbl_map(function(ft) return "!" .. ft end, consts.ignored_filetypes)
 ---@type LazySpec
 return {
   "NvChad/nvim-colorizer.lua",
   cond = vim.o.termguicolors,
+  ---@type colorizer.NewOptions
+  ---@diagnostic disable: missing-fields
   opts = {
-    filetypes = {
-      "*",
-      unpack(ignored),
-    },
-    user_default_options = {
-      names = false,
-      RRGGBBAA = true,
-      AARRGGBB = true,
-      css_fn = true,
+    options = {
+      filetypes = { "*", unpack(ignored) },
+      parsers = {
+        names = { enable = false },
+        tailwind = {
+          enable = true,
+          lsp = true,
+          update_names = true,
+        },
+        hex = {
+          enable = true,
+          rrggbbaa = true,
+          aarrggbb = true,
+        },
+        css_fn = true,
+      },
     },
   },
+  ---@diagnostic enable: missing-fields
   event = "LazyFile",
 }
