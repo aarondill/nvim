8144651ca2cb477d14db64285ca0773308ea93ad
Aaron Dill <aaronsacks2006@gmail.com>
2026-08-13 16:02:40 -0500

    fix: rust analyzer run on save

--- lsp/rust_analyzer.lua
+++ lsp/rust_analyzer.lua
@@ -1,8 +1,17 @@
 return { ---@type vim.lsp.Config
   settings = {
     ["rust-analyzer"] = {
+      checkOnSave = {
+        command = "clippy",
+      },
       check = {
         command = "clippy", -- Use clippy to check the code
+      },
+      diagnostics = {
+        enable = true,
+        experimental = {
+          enable = true,
+        },
       },
     },
   },
